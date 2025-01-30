524621e5b2c11d98f9495a423dfc9e6ee66e773f
Yaroslav <warlok38@mail.ru>
2025-01-30 19:00:03 +0700

    feat: lesson-10.1_mongo-agregations

--- src/product/product.model.ts
+++ src/product/product.model.ts
@@ -1,17 +1,50 @@
-export class ProductModel {
-  _id: number;
-  image: string;
-  title: string;
-  price: number;
-  oldPrice: number;
-  credit: number;
-  calculatedRating: number;
-  description: string;
-  advantages: string;
-  disAdvantages: string;
-  categories: string[];
-  tags: string[];
-  characteristics: {
-    [key: string]: string;
-  };
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+
+class ProductCharacteristic {
+  @Prop()
+  name: string;
+
+  @Prop()
+  value: string;
 }
+
+@Schema()
+export class ProductModel {
+  @Prop()
+  _id: number;
+
+  @Prop()
+  image: string;
+
+  @Prop()
+  title: string;
+
+  @Prop()
+  price: number;
+
+  @Prop()
+  oldPrice?: number;
+
+  @Prop()
+  credit: number;
+
+  @Prop()
+  description: string;
+
+  @Prop()
+  advantages: string;
+
+  @Prop()
+  disAdvantages: string;
+
+  @Prop()
+  categories: string[];
+
+  @Prop()
+  tags: string[];
+
+  @Prop({ type: () => [ProductCharacteristic], _id: false })
+  characteristics: ProductCharacteristic[];
+}
+
+export const ProductSchema = SchemaFactory.createForClass(ProductModel);
--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -1,7 +1,19 @@
 import { Module } from '@nestjs/common';
 import { ProductController } from './product.controller';
+import { ProductService } from './product.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ProductModel, ProductSchema } from './product.model';
 
 @Module({
   controllers: [ProductController],
+  providers: [ProductService],
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: ProductModel.name,
+        schema: ProductSchema,
+      },
+    ]),
+  ],
 })
 export class ProductModule {}
