3a40c995f576d8529f327a64364d4e717a5c93c3
Yaroslav <warlok38@mail.ru>
2025-01-30 17:17:02 +0700

    feat: lesson-9.7_auth test e2e

--- src/auth/auth.consts.ts
+++ src/auth/auth.consts.ts
@@ -2,4 +2,4 @@ export const ALREADY_REGISTERED_ERROR =
   'Такой пользователь уже был зарегистирован';
 
 export const USER_NOT_FOUND_ERROR = 'Пользователь с таким email не найден';
-export const WRONG_PASSWORD_ERROR = 'Пользователь с таким email не найден';
+export const WRONG_PASSWORD_ERROR = 'Неверный пароль';
